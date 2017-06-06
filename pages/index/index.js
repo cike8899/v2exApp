c70d8e4c89137c08c00da29d4737e57552593d6e
cike8899 <18267850322.com>
2017-06-07 00:47:46 +0800

    add about apge

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,22 +4,28 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    about: "关于"
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
+    })
+  },
+  handleAboutClick() {
+    wx.navigateTo({
+      url: '../about/about',
     })
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
