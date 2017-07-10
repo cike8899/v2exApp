82d1dcf8887e78062008884709744104961d5bfa
Bill Chen <18267850322@163.com>
2017-07-10 18:31:45 +0800

    alter sidebar

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,20 @@ Page({
     userInfo: {},
     about: "关于",
     sideOpen: false,
-    sideData: this.buildSideData()
+    sideData: [
+      "浏览",
+      "节点",
+      "设置",
+      "反馈",
+    ],
+    tabs: ['全部', '技术', '创意', '好玩', 'Apple', '酷工作'],
+    stv: {
+      windowWidth: 0,
+      lineWidth: 0,
+      offset: 0,
+      tStart: false
+    },
+    activeTab: 0
   },
   //事件处理函数
   bindViewTap: function () {
@@ -42,10 +55,18 @@ Page({
       url: skipTo,
     })
   },
+  handleSideClick() {
+    wx.navigateTo({
+      url: '../sidebar/index',
+    })
+  },
   showSidebar(e) {
-    console.info(e);
+    // console.info(e);
     var open = !this.data.sideOpen;
     this.setData({ sideOpen: open });
+  },
+  hideSidebar() {
+    this.setData({ sideOpen: false });
   },
   onLoad: function () {
     console.log('onLoad')
@@ -57,13 +78,90 @@ Page({
         userInfo: userInfo
       })
     })
+    try {
+      let {tabs} = this.data;
+      var res = wx.getSystemInfoSync()
+      this.windowWidth = res.windowWidth;
+      this.data.stv.lineWidth = this.windowWidth / this.data.tabs.length;
+      this.data.stv.windowWidth = res.windowWidth;
+      this.setData({ stv: this.data.stv })
+      this.tabsCount = tabs.length;
+    } catch (e) {
+    }
   },
-  buildSideData() {
-    return [
-      "浏览",
-      "节点",
-      "设置",
-      "反馈",
-    ]
+  handleSideItemTap() {
+    console.info("ddd");
+  },
+  handlerStart(e) {
+    let {clientX, clientY} = e.touches[0];
+    this.startX = clientX;
+    this.tapStartX = clientX;
+    this.tapStartY = clientY;
+    this.data.stv.tStart = true;
+    this.tapStartTime = e.timeStamp;
+    this.setData({ stv: this.data.stv })
+  },
+  handlerMove(e) {
+    let {clientX, clientY} = e.touches[0];
+    let {stv} = this.data;
+    let offsetX = this.startX - clientX;
+    this.startX = clientX;
+    stv.offset += offsetX;
+    if (stv.offset <= 0) {
+      stv.offset = 0;
+    } else if (stv.offset >= stv.windowWidth * (this.tabsCount - 1)) {
+      stv.offset = stv.windowWidth * (this.tabsCount - 1);
+    }
+    this.setData({ stv: stv });
+  },
+  handlerCancel(e) {
+
+  },
+  handlerEnd(e) {
+    let {clientX, clientY} = e.changedTouches[0];
+    let endTime = e.timeStamp;
+    let {tabs, stv, activeTab} = this.data;
+    let {offset, windowWidth} = stv;
+    //快速滑动
+    if (endTime - this.tapStartTime <= 300) {
+      //向左
+      if (Math.abs(this.tapStartY - clientY) < 50) {
+        if (this.tapStartX - clientX > 5) {
+          if (activeTab < this.tabsCount - 1) {
+            this.setData({ activeTab: ++activeTab })
+          }
+        } else {
+          if (activeTab > 0) {
+            this.setData({ activeTab: --activeTab })
+          }
+        }
+        stv.offset = stv.windowWidth * activeTab;
+      } else {
+        //快速滑动 但是Y距离大于50 所以用户是左右滚动
+        let page = Math.round(offset / windowWidth);
+        if (activeTab != page) {
+          this.setData({ activeTab: page })
+        }
+        stv.offset = stv.windowWidth * page;
+      }
+    } else {
+      let page = Math.round(offset / windowWidth);
+      if (activeTab != page) {
+        this.setData({ activeTab: page })
+      }
+      stv.offset = stv.windowWidth * page;
+    }
+    stv.tStart = false;
+    this.setData({ stv: this.data.stv })
+  },
+  _updateSelectedPage(page) {
+    let {tabs, stv, activeTab} = this.data;
+    activeTab = page;
+    this.setData({ activeTab: activeTab })
+    stv.offset = stv.windowWidth * activeTab;
+    this.setData({ stv: this.data.stv })
+  },
+  handlerTabTap(e) {
+    this._updateSelectedPage(e.currentTarget.dataset.index);
   }
 })
